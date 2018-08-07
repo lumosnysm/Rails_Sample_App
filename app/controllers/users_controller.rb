bd92ed6380b0b02daa3e214b05504afdcd687782
LongVH <lumosnysm@gmail.com>
2018-08-07 21:05:01 +0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  #test
   before_action :logged_in_user, except: %i(show new create)
   before_action :correct_user, only: %i(edit update)
   before_action :admin_user, only: %i(destroy)
